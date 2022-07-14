e9dcc1aedde44a06a3ffc720ead17f55496dc716
João Costa <primatajoao@gmail.com>
2022-07-14 12:20:13 -0300

    add: exercicio_python

--- projetos-individuais/joaocosta/main.py
+++ projetos-individuais/joaocosta/main.py
@@ -3,4 +3,7 @@ numero_de_palavras_no_titulo=(titulo.split())
 mostrar_variaveis=False
 
 if mostrar_variaveis:
+    print('o valor das variáveis é:')
     print(titulo,numero_de_palavras_no_titulo)
+else:
+    print('o valor das variáveis não será mostrado')
