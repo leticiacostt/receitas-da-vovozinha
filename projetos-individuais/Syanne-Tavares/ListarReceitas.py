21ae13974acaf5e6f3c9818b83aa96c086485356
syanne <syannekaroline@gmail.com>
2022-07-17 15:32:12 -0300

    fix: ajustado caminho da pasta receitas

--- projetos-individuais/Syanne-Tavares/ListarReceitas.py
+++ projetos-individuais/Syanne-Tavares/ListarReceitas.py
@@ -1,10 +1,14 @@
 from turtle import clear
 import yaml
 import os
+from pathlib import Path
+
 #mostra as receitas contidas na pasta receitas
 def ListarReceitas(pasta_receitas):
+    #utiliza a classe Path pra que o sistema operacional reconheça o caminho do arquivo 
+    caminho_arquivo= Path(pasta_receitas)
     #acessa os arquivos da pasta 
-    for diretorio, subpastas, arquivos in os.walk(f'{pasta_receitas}'):
+    for diretorio, subpastas, arquivos in os.walk(f'{caminho_arquivo}'):
         for arquivo in arquivos:
             print("\033[33mArquivo da receita: ",os.path.join(diretorio, arquivo),"\033[m")
             #realiza a leitura do arquivo
@@ -17,4 +21,4 @@ def ListarReceitas(pasta_receitas):
                     print(f'\033[34m{chave}\033[m : {valor} \n')
                 print("*"*100)
 
-ListarReceitas("./receitas")
+ListarReceitas(input("Insira o caminho da pasta que contém as receitas: "))
