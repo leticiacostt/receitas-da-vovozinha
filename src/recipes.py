c0fb45eb578ff670b5ece3de449cf65bacfaa08a
Rafael Picanço <cpicanco@users.noreply.github.com>
2022-07-18 10:55:24 -0300

    refac: renomeia funções de receitas

--- src/recipes.py
+++ src/recipes.py
@@ -11,24 +11,24 @@ except ImportError:
 
 from file_methods import get_recipe_filenames
 
-def load(receipt_filename):
+def load_recipe(receipt_filename):
     print('loading receipt:', receipt_filename)
     with open(receipt_filename, mode='r', encoding='utf8') as recipe_stream:
         return yaml.load(recipe_stream, Loader=Loader)
 
-def dump(receipt_dictionary):
+def dump_recipe(receipt_dictionary):
     return yaml.dump(receipt_dictionary, encoding='utf8', Dumper=Dumper, default_flow_style=False)
 
 def print_all_as_dictionary():
     for filename in get_recipe_filenames():
         print('*'*100)
-        print(load(filename))
+        print(load_recipe(filename))
         print('*'*100)
 
 def print_all_as_string():
     for filename in get_recipe_filenames():
         print('*'*100)
-        print(dump(load(filename)))
+        print(dump_recipe(load_recipe(filename)))
         print('*'*100)
 
 if __name__ == '__main__':
